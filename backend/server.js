3239a895e4d7539beccabd4b0597b456144298ea
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2024-07-11 18:19:14 -0400

    init

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to DB & Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
